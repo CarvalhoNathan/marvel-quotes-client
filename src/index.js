70a2abea0466f3bd0053dffe9f0354a8af42c093
CarvalhoNathan <fariasnathan75@gmail.com>
2022-02-09 13:45:21 -0300

    Adding Marvel image

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { GlobalStyle } from './components';
+import { GlobalStyle } from './components/globalStyle';
 import { App } from './pages';
 
 ReactDOM.render(
