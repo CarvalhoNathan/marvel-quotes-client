cc447d168ab2d5480a20eced925b0f2ecbafa8ea
CarvalhoNathan <fariasnathan75@gmail.com>
2022-02-09 13:55:40 -0300

    Styles and initial components

--- src/pages/app/App.js
+++ src/pages/app/App.js
@@ -1,11 +1,23 @@
+import styled from 'styled-components';
 import marvelImg from '../../images/avengers.png';
 
 export function App() {
   return (
-    <div>
-      <p>quote xpto -Speaker</p>
-      <button>Quote marvel</button>
-      <img src={marvelImg} alt="Marvel heores" />
-    </div>
-  )
+    <Content>
+      <MarvelImg src={marvelImg} alt="Marvel avengers" />
+    </Content>
+  );
 }
+
+const Content = styled.div`
+  height: 100vh;
+  padding: 0 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const MarvelImg = styled.img`
+  max-width: 50vw;
+  align-self: flex-end;
+`;
