71c7d507d380b97cdc458ec83141236265347f47
CarvalhoNathan <fariasnathan75@gmail.com>
2022-02-12 19:16:45 -0300

    Updating sound button and hooks

--- src/pages/app/App.js
+++ src/pages/app/App.js
@@ -1,20 +1,38 @@
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import styled from 'styled-components';
 import marvelImg from '../../images/avengers.png';
 import { Quotes } from '../../components/';
 import { getQuote } from '../../services';
+import marvelSound from '../../sounds/intro.mp3';
+
+const audio = new Audio(marvelSound);
 
 export function App() {
-  const [ quoteState, setQuoteState ] = useState({ 
-    quote: 'ok', 
-    speaker: 'Speaker'
+  const isMounted = useRef(true);
+
+  console.log(isMounted);
+
+  const [quoteState, setQuoteState] = useState({ 
+    quote: 'loading....', 
+    speaker: 'loading speaker....'
   });
   
   const onUpdate = async () => {
     const quote = await getQuote();
-
-    setQuoteState(quote);
+    
+    if (isMounted.current) {
+      audio.play();
+      setQuoteState(quote);
+    }
   };
+
+  useEffect(() => {
+    onUpdate();
+
+    return () => {
+      isMounted.current = false;
+    };
+  }, []);
   
   return (
     <Content>
--- src/pages/app/App.test.js
+++ src/pages/app/App.test.js
@@ -20,7 +20,7 @@ test('renders the app with a button, a quote and a button', () => {
 
   const buttonEl = screen.getByRole('button');
   const imageEl = screen.getByRole('img');
-  const textEl = screen.getByText(/Speaker/);
+  const textEl = screen.getByText(/loading speaker/);
 
   expect(buttonEl).toBeInTheDocument();
   expect(imageEl).toBeInTheDocument();
@@ -37,4 +37,12 @@ test('calls api on button click and update its text', async () => {
   const quoteEl = await screen.findByText(response.quote);
 
   expect(quoteEl).toBeInTheDocument();
-})
+});
+
+test('calls api on start and renders it response', async () => {
+  render(<App />);
+  
+  const quoteEl = await screen.findByText(response.quote);
+
+  expect(quoteEl).toBeInTheDocument();
+});
