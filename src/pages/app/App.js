b7fb9930776256bc7cc270673d6acac3cc4a7ce5
CarvalhoNathan <fariasnathan75@gmail.com>
2022-02-12 01:38:46 -0300

    Update API

--- src/pages/app/App.js
+++ src/pages/app/App.js
@@ -1,11 +1,24 @@
+import { useState } from 'react';
 import styled from 'styled-components';
 import marvelImg from '../../images/avengers.png';
 import { Quotes } from '../../components/';
+import { getQuote } from '../../services';
 
 export function App() {
+  const [ quoteState, setQuoteState ] = useState({ 
+    quote: 'ok', 
+    speaker: 'Speaker'
+  });
+  
+  const onUpdate = async () => {
+    const quote = await getQuote();
+
+    setQuoteState(quote);
+  };
+  
   return (
     <Content>
-      <Quotes quote={'ok'} speaker={'Speaker'} />
+      <Quotes {...quoteState} onUpdate={onUpdate} />
       <MarvelImg src={marvelImg} alt="Marvel avengers" />
     </Content>
   );
